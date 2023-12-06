441f837a50ab8757ce09ca23474e194e6db07944
officialCaesardev <116649209+officialCaesardev@users.noreply.github.com>
2023-12-06 06:19:41 +0530

    init

---
+++ src/app/page.tsx
@@ -0,0 +1,11 @@
+import React from 'react';
+
+const Home = () => {
+  return (
+    <div className="p-5">
+      <h1 className="text-center">Welcome To Next JS</h1>
+    </div>
+  );
+};
+
+export default Home;
